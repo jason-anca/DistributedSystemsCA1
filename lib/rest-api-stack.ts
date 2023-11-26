70052f513c3612565cf0be6091707727aeec7988
Jason Anca <jason_anca@hotmail.com>
2023-11-26 13:56:19 +0000

    Added Delete movie endpoint - code was taken from previously completed Severless REST lab

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -67,6 +67,18 @@ export class RestAPIStack extends cdk.Stack {
       },
     });
 
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
     new custom.AwsCustomResource(this, "moviesddbInitData", {
       onCreate: {
         service: "DynamoDB",
@@ -87,6 +99,7 @@ export class RestAPIStack extends cdk.Stack {
     moviesTable.grantReadData(getMovieByIdFn)
     moviesTable.grantReadData(getAllMoviesFn)
     moviesTable.grantReadWriteData(newMovieFn)
+    moviesTable.grantReadWriteData(deleteMovieFn)
 
     const api = new apig.RestApi(this, "RestAPI", {
       description: "demo api",
@@ -120,6 +133,11 @@ export class RestAPIStack extends cdk.Stack {
     moviesEndpoint.addMethod(
       "POST",
       new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
+
+    movieEndpoint.addMethod(
+      "DELETE",
+      new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
     );
   }
 }
